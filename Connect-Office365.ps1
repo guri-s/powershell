94da073163f6d83819d85faa1ac36d67b38adacd
gurinder-sandhar <38274495+gurinder-sandhar@users.noreply.github.com>
2018-04-11 16:23:39 +1000

    Update Connect-Office365.ps1

--- Connect-Office365.ps1
+++ Connect-Office365.ps1
@@ -1,3 +1,4 @@
+Import-Module MsOnline
 Set-ExecutionPolicy RemoteSigned
 $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication  Basic -AllowRedirection
