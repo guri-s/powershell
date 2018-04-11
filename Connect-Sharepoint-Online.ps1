be53af631af3e2cf37b01a7502a456c9698ac9fc
gurinder-sandhar <38274495+gurinder-sandhar@users.noreply.github.com>
2018-04-11 16:16:21 +1000

    Update Connect-Sharepoint-Online.ps1

--- Connect-Sharepoint-Online.ps1
+++ Connect-Sharepoint-Online.ps1
@@ -1,5 +1,5 @@
 #Connect to Sharepoint Online
 $adminUPN="@.onmicrosoft.com"
-$orgName="your-SP-organisation-name"
-$userCredential = Get-Credential -UserName $adminUPN -Message "Type the password."
+$orgName="your-SP-organisation-name e.g. mycompanyname.sharepoint.com " 
+$userCredential = Get-Credential -UserName $adminUPN -Message "Type your password."
 Connect-SPOService -Url https://$orgName-admin.sharepoint.com -Credential $userCredential
