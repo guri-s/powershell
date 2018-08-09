910fe3e3383d50d8721744086c93fa7d4825f123
guri-s <38274495+guri-s@users.noreply.github.com>
2018-08-09 14:18:44 +1000

    Update Change-Owner-SP-Site.ps1

--- Change-Owner-SP-Site.ps1
+++ Change-Owner-SP-Site.ps1
@@ -1,4 +1,10 @@
-$tenant =  “https://.sharepoint.com“
-$site = “https://.sharepoint.com/teams/test”
-$user =”@.onmicrosoft.com” # This will be the UPN for the user who will be added as the administrator.
-Set-SPOUser -Site $site -LoginName username@vigx.onmicrosoft.com -IsSiteCollectionAdmin $true
+#Load VB#
+[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') 
+#Asks about sitename
+$Site = [Microsoft.VisualBasic.Interaction]::InputBox("Which site would you like add an admin to", "Site URL", "") 
+$tenant =  “https://tenancyname.sharepoint.com“
+##$site = “https://bob.sharepoint.com/teams/test”
+$user = [Microsoft.VisualBasic.Interaction]::InputBox("Who do you want to add as Owner?", "Enter UPN", "") 
+##$user =”@.onmicrosoft.com” # This will be the UPN for the user who will be added as the administrator.
+Set-SPOUser -Site $site -LoginName $user -IsSiteCollectionAdmin $true
+[Microsoft.VisualBasic.Interaction]::MsgBox("Owner Added!", "OKOnly,SystemModal,Information", "Don't forget to thank Gurinder!")
